be4ea89dfc4dc33f2271d970c0e2a7389b983941
Luiz Eduardo Falcão <luizeduardoster@gmail.com>
2025-05-13 17:23:37 -0300

    First commit

---
+++ omnihub-core/src/main/java/com/falcao/omnihub/omnihub_core/OmniHubApplication.java
@@ -0,0 +1,13 @@
+package com.falcao.omnihub.omnihub_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniHubApplication.class, args);
+	}
+
+}
---
+++ omnihub-core/src/test/java/com/falcao/omnihub/omnihub_core/OmniHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falcao.omnihub.omnihub_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
